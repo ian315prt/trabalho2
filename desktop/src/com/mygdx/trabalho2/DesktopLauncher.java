b22248bea0a8e0e028e157a2b329c1bfd44439ba
Ian Irion <ianpratis9@gmail.com>
2022-11-29 22:22:35 -0300

    imports do libGDX

---
+++ desktop/src/com/mygdx/trabalho2/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.trabalho2;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.trabalho2.GameMain;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Trabalho2");
+		new Lwjgl3Application(new GameMain(), config);
+	}
+}
